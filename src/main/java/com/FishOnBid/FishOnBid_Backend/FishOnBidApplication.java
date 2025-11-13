ceb14e625db6bf8e03a56b64058541d7f273f0d9
Antony Vivin <vivinsonf@gmail.com>
2025-11-13 22:17:12 +0530

    Feat : Mysql connection and PostMan Verification Done!

---
+++ src/main/java/com/FishOnBid/FishOnBid_Backend/FishOnBidApplication.java
@@ -0,0 +1,13 @@
+package com.FishOnBid.FishOnBid_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishOnBidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishOnBidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FishOnBid/FishOnBid_Backend/FishOnBidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FishOnBid.FishOnBid_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishOnBidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
